b59ef59314a446c7f4d788988fc1ed324a04be69
Igor Moskvitin <i.moskvitin@2gis.ru>
2019-10-18 14:12:37 +0700

    CR

--- src/background.js
+++ src/background.js
@@ -1,17 +1,17 @@
 var clickHandler = function (e) {
-	chrome.storage.sync.get({
+    chrome.storage.sync.get({
         country: 'ru'
     }, function(items) {
-		var searchUrl = 'https://2gis.' + items.country + '/search/';
-		if (e.selectionText) {
-			searchUrl += encodeURI(e.selectionText);
-		}
-		chrome.tabs.create({ "url": searchUrl });
+        var searchUrl = 'https://2gis.' + items.country + '/search/';
+        if (e.selectionText) {
+            searchUrl += encodeURI(e.selectionText);
+        }
+        chrome.tabs.create({ "url": searchUrl });
     });
 };
 
 chrome.contextMenus.create({
-	"title": "Найти в 2ГИС",
-	"contexts": ["page", "selection"],
-	"onclick": clickHandler
+    "title": "Найти в 2ГИС",
+    "contexts": ["page", "selection"],
+    "onclick": clickHandler
 });
--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-	chrome.storage.sync.get({
-		country: 'ru'
-	  }, function(items) {
-		chrome.tabs.executeScript(null, { "code": "window.getSelection().toString()" }, function (selection) {
-			if (selection && selection[0] != "") {
-				var url = 'https://2gis.' + items.country + '/search/' + encodeURI(selection[0]);
-				chrome.tabs.create({ 'url': url });
-			}
-			else
-				document.getElementById("output")
-					.innerHTML = "<p>Необходимо выделить текст или можете перейти на <a href='https://2gis." + items.country + " target='_blank'>www.2gis." + items.country + "</a></p>";
-		});
-	  });
+﻿document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.sync.get({
+        country: 'ru'
+      }, function(items) {
+        chrome.tabs.executeScript(null, { "code": "window.getSelection().toString()" }, function (selection) {
+            if (selection && selection[0] != "") {
+                var url = 'https://2gis.' + items.country + '/search/' + encodeURI(selection[0]);
+                chrome.tabs.create({ 'url': url });
+            }
+            else
+                document.getElementById("output")
+                    .innerHTML = "<p>Необходимо выделить текст, либо перейти на <a href='https://2gis." + items.country + " target='_blank'>www.2gis." + items.country + "</a></p>";
+        });
+      });
 
-	  document.querySelector('#go-to-options').addEventListener('click', function() {
-		if (chrome.runtime.openOptionsPage) {
-		  chrome.runtime.openOptionsPage();
-		} else {
-		  window.open(chrome.runtime.getURL('options.html'));
-		}
-	  });
+      document.querySelector('#go-to-options').addEventListener('click', function() {
+        if (chrome.runtime.openOptionsPage) {
+          chrome.runtime.openOptionsPage();
+        } else {
+          window.open(chrome.runtime.getURL('options.html'));
+        }
+      });
 });
